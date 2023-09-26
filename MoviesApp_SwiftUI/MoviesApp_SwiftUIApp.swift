8cd93632875054033fa146f9954c6c4e836f4d50
pinaralper <ppinaralper@gmail.com>
2023-09-26 14:26:54 +0300

    Initial Commit

---
+++ MoviesApp_SwiftUI/MoviesApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp_SwiftUIApp.swift
+//  MoviesApp_SwiftUI
+//
+//  Created by PINAR KALKAN on 26.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
